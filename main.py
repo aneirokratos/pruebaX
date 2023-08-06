2b8c3ffbcf256f8db4d7944d89aa430506ea1a52
aneirokratos <aneirokratos@gmail.com>
2023-08-06 12:45:28 -0500

    HTML 2

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def welcome_page():
     return """
     <html>
         <head>
-            <title>Bienvenido</title>
+            <title>Proyecto individual</title>
         </head>
         <body>
-            <h1>Bienvenido a mi aplicación</h1>
-            <form action="/respuesta" method="post">
+            <h1>Aplicación de películas de Carlos Aneiro Pérez</h1>
+ <form action="/respuesta" method="post">
                 <label for="respuesta1">Respuesta 1:</label>
                 <input type="text" id="respuesta1" name="respuesta1"><br><br>
+                <input type="submit" value="Enviar">
+            </form>
+            
+            <form action="/respuesta2" method="post">
                 <label for="respuesta2">Respuesta 2:</label>
                 <input type="text" id="respuesta2" name="respuesta2"><br><br>
                 <input type="submit" value="Enviar">
@@ -24,15 +28,28 @@ def welcome_page():
     """
 
 @app.post("/respuesta", response_class=HTMLResponse)
-def respuesta_page(respuesta1: str, respuesta2: str):
+def respuesta_page(respuesta1: str):
     return f"""
     <html>
         <head>
             <title>Respuestas</title>
         </head>
         <body>
-            <h1>Tus respuestas:</h1>
+            <h1>Tu respuesta 1:</h1>
             <p>Respuesta 1: {respuesta1}</p>
+        </body>
+    </html>
+    """
+
+@app.post("/respuesta2", response_class=HTMLResponse)
+def respuesta2_page(respuesta2: str):
+    return f"""
+    <html>
+        <head>
+            <title>Respuestas</title>
+        </head>
+        <body>
+            <h1>Tu respuesta 2:</h1>
             <p>Respuesta 2: {respuesta2}</p>
         </body>
     </html>
