54e650fc9d651cdcff85290606e384d52fafc74d
aneirokratos <aneirokratos@gmail.com>
2023-08-09 17:33:56 -0500

    score_titulo

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ ruta_archivo = 'https://drive.google.com/uc?id=1-XkhEhJjFk4UsLy_EaT3yUa0BXjAnErv
 
 df = pd.read_csv(ruta_archivo, parse_dates=['release_date'])
 
-app = FastAPI()
+app = FastAPI(title='Proyecto Individual Henry Data science',
+            description='Carlos Aneiro Pérez')
 
 @app.get("/")
 def welcome_page():
@@ -22,8 +23,16 @@ def cantidad_filmaciones_mes(mes):
 
 
 @app.post("/filmaciones/dia")
-def cantidad_filmaciones_dia(dia):
+def cantidad_filmaciones_dia(dia:str):
     data_filtrado = df[df['release_date'].dt.day_name(
         locale='es') == dia]
     cantidad_dia = len(data_filtrado)
     return str(f"La cantidad de películas estrenadas en el dìa es {cantidad_dia}")
+
+@app.get('/score_titulo/{titulo}')
+def score_titulo(titulo:str):
+   lista_peliculas_title = df[df['title'] == titulo].drop_duplicates(subset='title')    
+   titulo = str(lista_peliculas_title['title'].iloc[0])
+   año = str(lista_peliculas_title['release_year'].iloc[0])
+   score =str(lista_peliculas_title['popularity'].iloc[0])
+   return {'titulo':titulo, 'año':año, 'popularidad':score}
