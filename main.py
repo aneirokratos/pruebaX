a05c06a6789b6e44665326bc252614b6df41cfdd
aneirokratos <aneirokratos@gmail.com>
2023-08-09 18:41:17 -0500

    prueba_mes

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 import pandas as pd
 from fastapi import FastAPI
 from starlette.responses import HTMLResponse
-
-import pandas as pd
+import locale
 
 ruta_archivo = 'https://drive.google.com/uc?id=1-XkhEhJjFk4UsLy_EaT3yUa0BXjAnErv'
 
 df = pd.read_csv(ruta_archivo, parse_dates=['release_date'])
+df['release_date'] = pd.to_datetime(df['release_date'])
 
 app = FastAPI(title='Proyecto Individual Henry Data science',
             description='Carlos Aneiro Pérez')
+
+# Establecer configuración regional al inicio de la aplicación
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
 @app.get("/")
 def welcome_page():
     return ("Aplicación de películas de Carlos Aneiro Pérez")
 
-@app.get("/fimaciones/mes{mes}")
-def cantidad_filmaciones_mes(mes):
-    data_filtrado = df[df['release_date'].dt.month_name(locale='es') == mes]
-    cantidad = len(data_filtrado)
-    return str(f"La cantidad de películas estrenadas en el mes es {cantidad}")
+@app.get("/fimaciones/mes/{mes}")
+def cantidad_filmaciones_mes(mes: str):
+  
+    mes = mes.lower()
+    
+    meses = {
+    'enero': 1,
+    'febrero': 2,
+    'marzo': 3,
+    'abril': 4,
+    'mayo': 5,
+    'junio': 6,
+    'julio': 7,
+    'agosto': 8,
+    'septiembre': 9,
+    'octubre': 10,
+    'noviembre': 11,
+    'diciembre': 12}
 
+    mes_numero = meses[mes]
+
+    try:
+        month_filtered = df[df['release_date'].dt.month == mes_numero]
+    except (ValueError, KeyError, TypeError):
+        return None
+
+    month_unique = month_filtered.drop_duplicates(subset='id')
+    respuesta = month_unique.shape[0]
+
+    return {'La cantidad de películas estrenadas en el mes es':mes, 'es de':respuesta}
 
 @app.get("/filmaciones/dia")
 def cantidad_filmaciones_dia(dia:str):
@@ -38,7 +65,7 @@ def cantidad_filmaciones_dia(dia:str):
     lista_peliculas_day = df[df['release_date'].dt.day_name() == day].drop_duplicates(subset='id')
     respuesta = lista_peliculas_day.shape[0]
 
-    return {'dia':dia, 'cantidad':respuesta}
+    return {'La cantidad de películas estrenadas en el día':dia, 'es de':respuesta}
 
 @app.get('/score_titulo/{titulo}')
 def score_titulo(titulo:str):
