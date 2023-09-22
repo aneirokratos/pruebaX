84496ad264da3888b1e8206799db687bc5f9c124
aneirokratos <aneirokratos@gmail.com>
2023-09-22 18:01:16 -0500

    1

--- main.py
+++ main.py
@@ -1,125 +1,90 @@
 import pandas as pd
 from fastapi import FastAPI
 
-ruta_archivo = 'https://drive.google.com/uc?id=1-XkhEhJjFk4UsLy_EaT3yUa0BXjAnErv'
+ruta_archivo_1 = 'https://drive.google.com/file/d/1nIuN3jQ0YccfCVE-H79nmdiRPTeTRgrm/view?usp=sharing'
+ruta_archivo_2 = 'https://drive.google.com/file/d/1PXAN45iaP1DQQ1DVbICFujtxFW-p18ZO/view?usp=sharing'
+ruta_archivo_3 = 'https://drive.google.com/file/d/1c3Sd2z6H5ydCrxDu8PFVjcBAMLc_ty4b/view?usp=sharing'
+ruta_archivo_4 = 'https://drive.google.com/file/d/1IDMT7UcriqUOspiOJIOw8SGtiBe05yai/view?usp=drive_link'
 
-df = pd.read_csv(ruta_archivo, parse_dates=['release_date'])
-df['release_date'] = pd.to_datetime(df['release_date'])
+# Leer el archivo CSV y guardarlo en un dataframe
+rank_genre = pd.read_csv(ruta_archivo_1)
+genre_data = pd.read_csv(ruta_archivo_2)
+df_steam = pd.read_csv(ruta_archivo_3)
+cant_items = pd.read_csv(ruta_archivo_4)
+
+# Puedes utilizar el dataframe como necesites en tu aplicación FastAPI
 
 app = FastAPI(title='Proyecto Individual Henry Data science',
             description='Carlos Aneiro Pérez')
+
+df_steam['release_date'] = pd.to_datetime(
+    df_steam['release_date'], errors='coerce')
 
 @app.get("/")
 def welcome_page():
     return ("Aplicación de películas de Carlos Aneiro Pérez")
 
-@app.get("/fimaciones/mes/{mes}")
-def cantidad_filmaciones_mes(mes: str):
-  
-    mes = mes.lower()
-    
-    meses = {
-    'enero': 1,
-    'febrero': 2,
-    'marzo': 3,
-    'abril': 4,
-    'mayo': 5,
-    'junio': 6,
-    'julio': 7,
-    'agosto': 8,
-    'septiembre': 9,
-    'octubre': 10,
-    'noviembre': 11,
-    'diciembre': 12}
-
-    mes_numero = meses[mes]
-
-    try:
-        month_filtered = df[df['release_date'].dt.month == mes_numero]
-    except (ValueError, KeyError, TypeError):
-        return None
-
-    month_unique = month_filtered.drop_duplicates(subset='id')
-    respuesta = month_unique.shape[0]
-
-    return {'La cantidad de películas estrenadas en el mes es':mes, 'es de':respuesta}
-
-@app.get("/filmaciones/dia")
-def cantidad_filmaciones_dia(dia:str):
-    days = {
-    'lunes': 'Monday',
-    'martes': 'Tuesday',
-    'miercoles': 'Wednesday',
-    'jueves': 'Thursday',
-    'viernes': 'Friday',
-    'sabado': 'Saturday',
-    'domingo': 'Sunday'}
-
-    day = days[dia.lower()]
-
-    lista_peliculas_day = df[df['release_date'].dt.day_name() == day].drop_duplicates(subset='id')
-    respuesta = lista_peliculas_day.shape[0]
-
-    return {'La cantidad de películas estrenadas en el día':dia, 'es de':respuesta}
-
-@app.get('/score_titulo/{titulo}')
-def score_titulo(titulo:str):
-   lista_peliculas_title = df[df['title'] == titulo].drop_duplicates(subset='title')    
-   titulo = str(lista_peliculas_title['title'].iloc[0])
-   año = str(lista_peliculas_title['release_year'].iloc[0])
-   score =str(lista_peliculas_title['popularity'].iloc[0])
-   return {'titulo':titulo, 'año':año, 'popularidad':score}
-
-@app.get('/votos_titulo/{votos}')
-def votos_titulo(titulo_de_la_filmacion:str):
-    data = df
-    pelicula = data[data['title'] == titulo_de_la_filmacion]
-    if pelicula.empty:
-        return "No se encontró información para la película: " + titulo_de_la_filmacion
-    else:
-        votos = pelicula['vote_count'].values[0]
-        promedio = pelicula['vote_average'].values[0]
-        if votos >= 2000:
-            return f"La película '{titulo_de_la_filmacion}' fue estrenada en el año {pelicula['release_year'].values[0]}. La misma cuenta con un total de {votos} valoraciones, con un promedio de {promedio}"
+@app.get("/Developer")
+def developer(df, developer):
+    # Filter data for the specified developer
+    df_dev = df_steam[df_steam['developer'] == developer]
+    # Get a list of unique years
+    years = df_steam['release_date'].dt.year.unique()
+    # Iterate over the years
+    percentages = []
+    for year in years:
+        # Filter data for the year
+        df_year = df_dev[df_dev['release_date'].dt.year == year]
+        # Convert 'price' column to string
+        df_year['price'] = df_year['price'].astype(str)
+        # Count the number of free contents
+        num_free = df_year[df_year['price'].str.lower(
+        ).str.contains('free')].shape[0]
+        # Calculate the percentage
+        if num_free == 0:
+            percentage = 0
         else:
-            return f"La película '{titulo_de_la_filmacion}' no cumple con la condición de tener al menos 2000 valoraciones."
+            percentage = (num_free / df_year.shape[0]) * 100
+        # Add the percentage to the list
+        percentages.append(f'{year}: {percentage:.2f}%')
+    return percentages
 
-@app.get('/actor/{actor}')
-def get_actor(nombre_actor):
-    lista_peli_actor = df[df['name_cast'].astype(str).str.contains(
-        nombre_actor, case=False, na=False)].drop_duplicates(subset='id')
-    if len(lista_peli_actor) > 0:
-        num_movies = len(lista_peli_actor)
-        total_return = lista_peli_actor['return'].sum()
-        average_return = round(total_return / num_movies, 2)
-        return f"El actor {nombre_actor} ha participado en {num_movies} películas, logrando un retorno total de {total_return} con un promedio de {average_return} por película."
-    else:
-        return f"No se encontraron datos del actor {nombre_actor}."
-    
-@app.get('/director/{director}')
-def get_director(nombre_director):
+@app.get("/userdata")
+def userdata(user_id):
+    # Filtrar el dataframe por el user_id especificado
+    user_data = cant_items[cant_items["user_id"] == user_id]
 
-    lista_peli_director = df[df['name_crew'].astype(str).str.contains(
-        nombre_director, case=False, na=False)].drop_duplicates(subset='title')
-    if len(lista_peli_director) > 0:
-        movies = lista_peli_director['title'].tolist()
-        release_years = lista_peli_director['release_year'].tolist()
-        returns = lista_peli_director['return'].tolist()
-        budgets = lista_peli_director['budget'].tolist()
-        revenues = lista_peli_director['revenue'].tolist()
-        return list(zip(movies, release_years, returns, budgets, revenues))
-    else:
-        return f"No records found for the director {nombre_director}."
+    # Obtener la cantidad de items y la suma de precios
+    item_count = len(user_data)
+    total_price = user_data["price"].sum()
+
+    return {
+        "Cantidad de items": item_count,
+        "Suma de precios": total_price
+    }
 
 
-@app.get('/recomendaciones/{recomendaciones}')
-def obtener_recomendaciones_peliculas(title):
-    # Obtener el género de la película de entrada
-    genero_pelicula = df[df['title'] == title]['genero_nombre'].values[0]
-    # Filtrar las películas que coinciden con el género de la película de entrada
-    peliculas_coincidentes = df[df['genero_nombre'] == genero_pelicula]
-    # Excluir la película de entrada de las recomendaciones
-    peliculas_recomendadas = peliculas_coincidentes[peliculas_coincidentes['title'] != title]
-    # Obtener los primeros 5 títulos de películas recomendadas
-    recomendaciones = peliculas_recomendadas['title'].head(5).tolist()
-    return recomendaciones
+@app.get('genre')
+def genre(genero):
+    # filtro el dataframe "rank_genre" para quesu columna "genres" sea igual a el dato que se ingresa
+    # a partir de esto se selecciona la columna "ranking" del conjunto resultante y se bloquea para obtener el valor
+    orden = rank_genre[rank_genre["genre"] == genero]["ranking"].iloc[0]
+    return {
+        "El género": genero,
+        "se encuentra en el puesto": orden
+    }
+
+
+@app.get('/userforgenre')
+def userforgenre(genero):
+    # se filtra el dataframe con la columna "genres" y se la iguala con el dato ingresado
+    genre_data = genre_data[genre_data["genre"] == genero]
+    # agrupo el genero por usuario y horas jugadas
+    user_hours = genre_data.groupby(["user_id", "user_url"])[
+        "playtime_forever"].sum().reset_index()
+    # ordena los valores de mayor a menor
+    user_hours = user_hours.sort_values(by="playtime_forever", ascending=False)
+    # extrae los primeros 5
+    top_5_users = user_hours.head(5)
+    return top_5_users
+
